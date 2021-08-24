5aab0504fefcb4dc4a291adb87457b9205f2b68b
Kz-m <52796744+Kz-m@users.noreply.github.com>
2021-08-24 09:07:14 +0900

    install devise 〜MVC(users)
    ・installed devise
    ・build users_controller
    ・create users_table

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :purchase_histories
+  
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :given_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :given_name_kana, presence: true
+  validates :date_of_birth, presence: true
+
+
+end
