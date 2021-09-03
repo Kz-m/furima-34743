80cbc610526cf08e03d978ab93ab8331086f93a1
Kz-m <52796744+Kz-m@users.noreply.github.com>
2021-09-04 08:05:31 +0900

    fill in views(half done), installed Activehash

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
   
-  #def show
-  #end 
+  def show
+  end 
 
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id) #TODO// lemme check item_params
+  end
 
 end
