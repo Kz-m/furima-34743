1c51b02c8213acf0e08970ed8ec43419a23138ac
Kz-m <52796744+Kz-m@users.noreply.github.com>
2021-09-16 12:32:33 +0900

    revise 3rd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :shop_item, only: [:show, :destroy]
+  before_action :shop_item, only: [:show]
 
   def index 
     @items = Item.all.order("created_at DESC")
@@ -25,20 +25,20 @@ class ItemsController < ApplicationController
   #def edit
   #end
 
-  def update
-    #if @item.save
-    #  redirect_to item_path
-    #end
-  end
+  #def update
+  #  if @item.save
+  #    redirect_to item_path
+  #  end
+  #end
   
-  def destroy
-    if current_user.id == @item.user_id
-      @item.destroy
-      redirect_to root_path
-    else
-      render :index
-    end
-  end
+  #def destroy
+  #  if current_user.id == @item.user_id
+  #    @item.destroy
+  #    redirect_to root_path
+  #  else
+  #    render :index
+  #  end
+  #end
 
 
   private
